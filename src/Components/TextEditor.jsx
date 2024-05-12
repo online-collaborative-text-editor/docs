6d6b47ba5b0b99c523b7ef34c0751dfcb0463e5e
FatemaKotb <Fatema.Kotb02@eng-st.cu.edu.eg>
2024-05-13 01:58:59 +0300

    Sockets initialization

--- src/Components/TextEditor.jsx
+++ src/Components/TextEditor.jsx
@@ -32,95 +32,106 @@ const saveButtonStyle = {
 
 const mockDatabase = [];
 const TextEditor = () => {
+    const location = useLocation();
+    
+    const file = location.state?.file;
+    let page = location.state?.page;
+    const docId = location.state?.id;
+
+    console.log("file is", file);
+    console.log("page is", page);
+    console.log("docId is", docId);
+
     const [socket, setSocket] = useState();
     const [quill, setQuill] = useState();
     useEffect(() => {
-        const s = io("http://localhost:3001");//server port 
+        const s = io('http://localhost:5000', {
+            query: {
+                username: localStorage.getItem('username'),
+                docId: docId
+            }
+        });
         setSocket(s);
         console.log("connected to server");
-        return () => {
-            s.disconnect();
-        }
+            return() => {
+    s.disconnect();
+}
     }, []);
 
-    const location = useLocation();
 
-    const file = location.state?.file;
-    let page = location.state?.page;
+useEffect(() => {
 
-    useEffect(() => {
-
-        const editor = document.querySelector('.ql-editor');
-        editor.innerHTML = file.content;
-        if (page === "viewed" && editor) {
-            editor.setAttribute('contenteditable', 'false');
-            editor.style.backgroundColor = 'lightgrey';
-        }
-
-    }, [page, file])
-    //send changes to server
-    useEffect(() => {
-        if (quill == null || socket == null) return
-        const handler = (delta, oldDelta, source) => {
-            if (source !== 'user') return
-            socket.emit('send-changes', delta)
-        }
-        quill?.on('text-change', handler)//event listener 
-        return () => {//cleanup 
-            quill?.off('text-change', handler)
-        }
-
-    }, [quill, socket])
-    //recieve changes from server 
-    useEffect(() => {
-        if (quill == null || socket == null) return
-        const handler = (delta, oldDelta, source) => {
-            quill.updateContents(delta)
-        }
-        socket?.on('receive-changes', handler)//event listener 
-        return () => {//cleanup 
-            socket?.off('receive-changes', handler)
-        }
-
-    }, [quill, socket])
-    const wrapperRef = useCallback((wrapper) => {
-        if (wrapper == null) return
-        wrapper.innerHTML = ""
-
-        const editor = document.createElement('div')
-        wrapper.append(editor)
-        const q = new Quill(editor, { theme: 'snow' })
-        setQuill(q);
-    }, [])
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const editor = document.querySelector('.ql-editor');
-        const content = editor.innerHTML;
-        const fileName = prompt('Enter the file name', "untitled");
-        mockDatabase.push({ name: fileName, content: content });
-        console.log(mockDatabase);
-        //TODO:http request to save the content 
-
+    const editor = document.querySelector('.ql-editor');
+    editor.innerHTML = file.content;
+    if (page === "viewed" && editor) {
+        editor.setAttribute('contenteditable', 'false');
+        editor.style.backgroundColor = 'lightgrey';
     }
-    return (
 
-        <div>
+}, [page, file])
+//send changes to server
+useEffect(() => {
+    if (quill == null || socket == null) return
+    const handler = (delta, oldDelta, source) => {
+        if (source !== 'user') return
+        socket.emit('send-changes', delta)
+    }
+    quill?.on('text-change', handler)//event listener 
+    return () => {//cleanup 
+        quill?.off('text-change', handler)
+    }
 
-            <AppBar
-                name={file?.name ? file.name : null}
-                page={page} />
+}, [quill, socket])
+//recieve changes from server 
+useEffect(() => {
+    if (quill == null || socket == null) return
+    const handler = (delta, oldDelta, source) => {
+        quill.updateContents(delta)
+    }
+    socket?.on('receive-changes', handler)//event listener 
+    return () => {//cleanup 
+        socket?.off('receive-changes', handler)
+    }
 
-            <div id="editorcontainer" ref={wrapperRef}></div>;
-            <form id="form" onSubmit={handleSubmit} >
-                <div style={{ textAlign: 'center', margin: '2rem' }}></div>
-                {page != "viewed" ? <button type="submit" style={saveButtonStyle}>Save</button> : null}
+}, [quill, socket])
+const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return
+    wrapper.innerHTML = ""
 
-            </form>
+    const editor = document.createElement('div')
+    wrapper.append(editor)
+    const q = new Quill(editor, { theme: 'snow' })
+    setQuill(q);
+}, [])
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const editor = document.querySelector('.ql-editor');
+    const content = editor.innerHTML;
+    const fileName = prompt('Enter the file name', "untitled");
+    mockDatabase.push({ name: fileName, content: content });
+    console.log(mockDatabase);
+    //TODO:http request to save the content 
 
-        </div>
+}
+return (
+
+    <div>
+
+        <AppBar
+            name={file?.name ? file.name : null}
+            page={page} />
+
+        <div id="editorcontainer" ref={wrapperRef}></div>;
+        <form id="form" onSubmit={handleSubmit} >
+            <div style={{ textAlign: 'center', margin: '2rem' }}></div>
+            {page != "viewed" ? <button type="submit" style={saveButtonStyle}>Save</button> : null}
+
+        </form>
+
+    </div>
 
 
-    )
+)
 };
 
 export default TextEditor;
